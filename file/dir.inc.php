92805782635c13fd7100388972fef29a56db6dd9
陶士涵 <630892807@qq.com>
2015-04-10 18:12:02 +0800

    基本功能完成

--- file/dir.inc.php
+++ file/dir.inc.php
@@ -9,22 +9,37 @@ header("content-Type: text/html; charset=Utf-8");
 
 define('PATH','file/');//定义管理的目录
 
-$data=readDirectory(PATH);
 
-$path=isset($_REQUEST['path']) ? $_REQUEST['path'] : PATH;
+
+if(isset($_REQUEST['path'])){
+  $path=str_replace("//",'/',urldecode($_REQUEST['path'])).'/';
+}else{
+  $path=PATH;
+}
 $act=isset($_REQUEST['act']) ? $_REQUEST['act'] : '';
 
-/*创建文件*/
-if($act=='createFile'){
-  $path=$_POST['path'];
-  $fileName=$_POST['fileName'];
-  $info=createFile($path.$fileName);
-  exit($info);
+/*验证一下*/
+$checkPath=explode('/',$path);
+if($checkPath[0].'/'!=PATH || !is_dir($path)){
+  jump('没有这个文件夹哦',false);
 }
 
+$data=readDirectory($path);
+/*创建文件*/
+if($act=='createFile'){
+  $fileName=$path.$_POST['fileName'];
+  $info=createFile($fileName);
+  exit($info);
+}
+/*创建文件夹*/
+if($act=='createDir'){
+  $dirName=$path.$_POST['dirName'];
+  $info=createDir($dirName);
+  exit($info);
+}
 /*查看文件*/
 if($act=='showContent'){
-  $filePath=PATH.$_GET['fileName'];
+  $filePath=$_GET['fileName'];
   $filePath=iconv("utf-8","gbk//IGNORE",$filePath);
 
   $str=showFile($filePath);
@@ -33,7 +48,7 @@ if($act=='showContent'){
 
 /*编辑文件*/
 if($act=='editContent'){
-  $filePath=PATH.$_GET['fileName'];
+  $filePath=$_GET['fileName'];
   $filePath=iconv("utf-8","gbk//IGNORE",$filePath);
 
   if(isImage($filePath)){
@@ -47,8 +62,8 @@ if($act=='editContent'){
 
 /*处理编辑文件*/
 if($act=='doEditContent'){
-  exit("sorry,怕有安全问题,关掉了");
-  $filePath=PATH.$_POST['fileName'];
+  // exit("sorry,怕有安全问题,关掉了");
+  $filePath=$_POST['fileName'];
   $filePath=iconv("utf-8","gbk//IGNORE",$filePath);
   $fileContent=stripcslashes($_POST['fileContent']);
 
@@ -66,18 +81,81 @@ if($act=='doEditContent'){
 
 }
 
+/*重命名文件*/
+if($act=='renameFile'){
+  $oldFileName=iconv("utf-8","gbk//IGNORE",$_POST['oldFileName']);
+  $newFileName=iconv("utf-8","gbk//IGNORE",$_POST['newFileName']);
+
+  if(!checkFileName($newFileName)){
+    exit('文件名中包含非法字符/,*,<,>,?,|');
+  }
+  if(file_exists($path.$newFileName)){
+    exit('文件名已存在,请重新填写文件名');
+  }
+  $res=rename($path.$oldFileName, $path.$newFileName);
+  if($res){
+    exit('文件名修改成功!');
+  }else{
+    exit('修改失败,请重新填写文件名!');
+  }
+}
+/*删除文件*/
+if($act=='delFile'){
+  $fileName=$_GET['fileName'];
+
+  if(is_dir($fileName)){
+    $res=delDir($fileName.'/');
+  }else{
+    $res=delFile($fileName);
+  }
+  
+  if($res){
+    exit('删除成功');
+  }else{
+    exit('删除失败');
+  }
+}
+/*下载文件*/
+if($act=='downFile'){
+  $fileName=urldecode($_GET['fileName']);
+  downFile($fileName);
+  exit;
+}
+/*上传文件*/
+if($act=='uploadFile'){
+  $type=$_FILES["uploadFileInput"]["type"];
+  $size=$_FILES["uploadFileInput"]["size"];
+  $tmp_name=$_FILES["uploadFileInput"]["tmp_name"];
+  $name=$_FILES["uploadFileInput"]["name"];
+  $name=iconv("utf-8","gbk//IGNORE",$name);
+
+ 
+  if($_FILES["uploadFileInput"]["error"]>0){
+    $info= "上传文件有误:".$_FILES["uploadFileInput"]["error"];
+  }
+  $dir=$_GET['path'];
+  $newPath=$dir."/{$name}";
+
+
+  if(move_uploaded_file($tmp_name,$newPath)){ 
+     jump('上传文件成功',true);
+  }else{
+     jump('上传文件失败',false);
+  }
+}
 /**
  * 读目录函数
  */
 function readDirectory($path){
+  $path=iconv("utf-8","gbk//IGNORE",$path);
   $handle=opendir($path);
   $arr=array();
   while(($item=readdir($handle))!==false){
     
     if($item!='.'&&$item!='..'){
-      
+
       $info=array();
-      $filePath=PATH.$item;
+      $filePath=$path.$item;
       $ext=filetype($filePath);
       $info['ext']=$ext;
 
@@ -95,16 +173,21 @@ function readDirectory($path){
       $info['ctime']=date("Y-m-d H:i:s",filectime($filePath));
 
       $info['name']=$item;
+      $info['showname']=iconv("gbk//IGNORE","utf-8",$info['name']);
 
       if(is_dir($filePath)){
         $info['type']='dir';
+        $info['name']=iconv("gbk//IGNORE","utf-8",$path.$info['name']);
+        $sumSize=0;global $sumSize;$info['size']=transBytes(getDirSize($filePath.'/'));//目录总大小
+        $arr['dir'][]=$info;
       }
       if(is_file($filePath)){
-        $info['file']='file';
+        $info['type']='file';
+        $info['name']=iconv("gbk//IGNORE","utf-8",$path.$info['name']);
+        $arr['file'][]=$info;
       }
 
-      $info['name']=iconv("gbk//IGNORE","utf-8",$info['name']);
-      $arr['file'][]=$info;
+      
     }
   }
   closedir($handle);
@@ -147,7 +230,6 @@ EOF;
  * 创建文件
  */
 function createFile($fileName){
-  exit("sorry,怕有安全问题,关掉了");
   if(preg_match('/[\/,\*,<,>,\?,\|]/',basename($fileName))) return '文件名中包含非法字符/,*,<,>,?,|';
   if(file_exists($fileName)) return '文件名已存在,请重命名后提交';
   $fileName=iconv("utf-8","gbk//IGNORE",$fileName);
@@ -157,6 +239,39 @@ function createFile($fileName){
     return '文件创建失败';
   }
 }
+/**
+ * 创建文件夹
+ */
+function createDir($fileName){
+  if(preg_match('/[\/,\*,<,>,\?,\|]/',basename($fileName))) return '文件夹名中包含非法字符/,*,<,>,?,|';
+  if(file_exists($fileName)) return '文件名夹已存在,请重命名后提交';
+  $fileName=iconv("utf-8","gbk//IGNORE",$fileName);
+  if(mkdir($fileName)){
+    return '文件夹创建成功';
+  }else{
+    return '文件夹创建失败';
+  }
+}
+/**
+ * 删除文件
+ */
+function delFile($fileName){
+  $fileName=iconv("utf-8","gbk//IGNORE",$fileName);
+  if(unlink($fileName)){
+    return true;
+  }else{
+    return false;
+  }
+}
+/**
+ * 下载文件
+ */
+function downFile($fileName){
+  header('Content-Disposition: attachment; filename="'.basename($fileName).'"'); 
+  header('Content-length:'.filesize($fileName));
+  readFile($fileName);
+}
+
 /**
  * 判断是否是图片
  */
@@ -169,4 +284,104 @@ function isImage($fileName){
       return false;
     }
 }
+/**
+ * 检测文件名的合法性
+ */
+function checkFileName($fileName){
+  if(preg_match('/[\/,\*,<,>,\?,\|]/',basename($fileName))){
+    return false;
+  }else{
+    return true;
+  }
+}
+/**
+ * 获取目录的文件总大小
+ */
+function getDirSize($path){
+  $sumSize=0;
+  global $sumSize;
+  $handle=opendir($path);
+
+  while(($item=readdir($handle))!==false){
+    
+    if($item!='.' && $item!='..'){
+      $filePath=$path.$item;
+      if(is_file($filePath)){
+        $sumSize+=filesize($filePath);
+      }elseif(is_dir($filePath)){
+        $func=__FUNCTION__;
+        $func($filePath.'/');
+      }
+    }
+  }
+
+  closedir($handle);
+  return $sumSize;
+}
+/**
+ * 删除文件夹
+ */
+function delDir($path){
+  $handle=opendir($path);
+  while(($item=readdir($handle))!==false){
+    
+    if($item!='.' && $item!='..'){
+      $filePath=$path.$item;
+      if(is_file($filePath)){
+        $filePath=str_replace('//','/',$filePath);
+        unlink($filePath);
+      }elseif(is_dir($filePath)){
+        $func=__FUNCTION__;
+        $func($filePath.'/');
+      }
+    }
+    
+  }
+
+  closedir($handle);
+  $res=rmdir($path);
+  if($res){
+    return true;
+  }else{
+    return flase;
+  }
+}
+/**
+ * 跳转函数
+ */
+function jump($msg,$status){
+    if($status){
+       $info="<h1>:)</h1><p class='success'>{$msg}</p>";
+    }else{
+      $info="<h1>:(</h1><p class='error'>{$msg}</p>";
+    }
+
+    $checkPath=explode('/',$_GET['path']);
+    if($checkPath[0].'/'!=PATH){
+      $_GET['path']=PATH;
+    }
+
+    $jumpHtml=<<<EOF
+<style type="text/css">
+*{ padding: 0; margin: 0; }
+body{ background: #fff; font-family: '微软雅黑'; color: #333; font-size: 16px; }
+.system-message{ padding: 24px 48px; }
+.system-message h1{ font-size: 100px; font-weight: normal; line-height: 120px; margin-bottom: 12px; }
+.system-message .jump{ padding-top: 10px}
+.system-message .jump a{ color: #333;}
+.system-message .success,.system-message .error{ line-height: 1.8em; font-size: 36px }
+.system-message .detail{ font-size: 12px; line-height: 20px; margin-top: 12px; display:none}
+</style>
+<div class="system-message">
+{$info}
+<p class="detail"></p>
+<p class="jump">
+回到文件列表: <a id="href" href="index.php?path={$_GET['path']}">跳转</a>
+</p>
+</div>
+EOF;
+  echo $jumpHtml;
+  exit;
+}
+
 ?>
--- file/index.php
+++ file/index.php
@@ -36,17 +36,17 @@ require 'dir.inc.php';
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">服务器文件管理系统v1.0</a>
+            <a class="navbar-brand" href="index.php">服务器文件管理系统v1.0</a>
           </div>
           <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
               <li class="active"><a href="index.php">主目录<span class="icon icon-small "><span class="icon-tpl-full"></span></span></a></li>
-              <li><a href="javascript:void(0)" data-toggle="modal" data-target="#createFile"><span class="icon icon-small "><span class="icon-folder"></span></span>新建文件夹</a></li>
+              <li><a href="javascript:void(0)" data-toggle="modal" data-target="#createDir"><span class="icon icon-small "><span class="icon-folder"></span></span>新建文件夹</a></li>
               <li><a href="javascript:void(0)" data-toggle="modal" data-target="#createFile"><span class="icon icon-small "><span class="icon-file"></span></span>新建文件</a></li>
-              <li><a href="index.php"><span class="icon icon-small "><span class="icon-arrowLeft"></span></span>返回上级</a></li>
+              <li><a href="javascript:window.history.go(-1)"><span class="icon icon-small "><span class="icon-arrowLeft"></span></span>返回上级</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-              <li><a href="#"><span class="icon icon-small "><span class="icon-upload"></span></span>上传文件</a></li>
+              <li><a href="#" data-toggle="modal" data-target="#uploadFile"><span class="icon icon-small "><span class="icon-upload"></span></span>上传文件</a></li>
             </ul>
           </div>
         </div>
@@ -57,13 +57,27 @@ require 'dir.inc.php';
         <h1>PHP文件管理器</h1>
         <p>基于php实现的web在线文件管理器,前端使用bootstrap框架和Cikonss图标样式库---陶士涵</p>
         <!--[if IE]><p>请尽量不要使用IE或IE内核的浏览器查看本页面, O(∩_∩)O~</p><![endif]-->
+        <p class="masthead-button-links">
+          <a class="btn btn-lg btn-primary btn-shadow" href="fileManage-master.zip" target="_blank" role="button">FileManage中文版(v1.0)</a>
+        </p>
+      </div>
 
+      <div class="roadPath">
+      文件路径: <a href="index.php">根目录</a>
+      <?php 
+      $temp=explode('/',$path);
+      $p='';
+      foreach($temp as $v){
+        $p.=$v.'/';
+        if($v!='') echo " &gt; <a href='index.php?path={$p}'>{$v}</a>";
+      }
+      ?>
       </div>
 
       <table class="table table-bordered table-hover">
         <tr class="active">
-          <td>名称</td>
           <td>类型</td>
+          <td>名称</td>
           <td>大小</td>
           <td>读</td>
           <td>写</td>
@@ -71,25 +85,59 @@ require 'dir.inc.php';
           <td>创建时间</td>
           <td>操作</td>
         </tr>
-        <?php foreach ($data['file'] as $k => $row) {?>
+        <?php if(!empty($data['dir'])){foreach ($data['dir'] as $k => $row) {?>
         <tr>
-          <td><?php echo $row['name'];?></td>
           <td><span class="icon icon-mid "><span class="<?php $ext=$row['type']=='dir' ?'icon-folder' : 'icon-file';echo $ext;?>"></span></span></td>
+          <td>
+          <div class="changeFileName">
+             <p class="oldRowFileName"><?php echo $row['showname'];?></p>
+             <p class="newRowFileName"></p>
+          </div>
+          </td>
           <td><?php echo $row['size'];?></td>
           <td><span class="icon icon-mid "><span class="<?php $ico=$row['readable']==true ?'icon-play' : 'icon-pause';echo $ico;?>"></span></span></td>
           <td><span class="icon icon-mid "><span class="<?php $ico=$row['writable']==true ?'icon-play' : 'icon-pause';echo $ico;?>"></span></span></td>
           <td><span class="icon icon-mid "><span class="<?php $ico=$row['executable']==true ?'icon-play' : 'icon-pause';echo $ico;?>"></span></span></td>
           <td><?php echo $row['ctime'];?></td>
           <td>
-            <a href="javascript:void(0)" data-url="index.php?act=showContent&fileName=<?php echo $row['name']?>" data-toggle="modal" data-target="#showContent" class="showContentBtn">[ 查看 ]</a>&nbsp;&nbsp;
-            <a href="javascript:void(0)" data-filename="<?php echo $row['name']?>" data-url="index.php?act=editContent&fileName=<?php echo $row['name']?>" data-toggle="modal" data-target="#editContent" class="editContentBtn">[ 编辑 ]</a>
-
+            <a href="index.php?path=<?php echo $row['name']?>" class="showContentBtn btn btn-info btn-sm">打开</a>&nbsp;&nbsp;
+           
+            <a href="javascript:void(0)" data-filename="<?php echo $row['name']?>" data-url="index.php?act=delFile&fileName=<?php echo $row['name']?>" data-toggle="modal" data-target="#delFileConfirm" class="delFileBtn  btn btn-danger btn-sm">删除</a>&nbsp;&nbsp;
+            
           </td>
         </tr>
-        <?php }?>
+        <?php }}?>
+
+
+        <?php if(!empty($data['file'])){foreach ($data['file'] as $k => $row) {?>
+        <tr>
+          <td><span class="icon icon-mid "><span class="<?php $ext=$row['type']=='dir' ?'icon-folder' : 'icon-file';echo $ext;?>"></span></span></td>
+          <td>
+          <div class="changeFileName">
+             <p class="oldRowFileName"><?php echo $row['showname'];?></p>
+             <p class="newRowFileName"></p>
+          </div>
+          </td>
+          <td><?php echo $row['size'];?></td>
+          <td><span class="icon icon-mid "><span class="<?php $ico=$row['readable']==true ?'icon-play' : 'icon-pause';echo $ico;?>"></span></span></td>
+          <td><span class="icon icon-mid "><span class="<?php $ico=$row['writable']==true ?'icon-play' : 'icon-pause';echo $ico;?>"></span></span></td>
+          <td><span class="icon icon-mid "><span class="<?php $ico=$row['executable']==true ?'icon-play' : 'icon-pause';echo $ico;?>"></span></span></td>
+          <td><?php echo $row['ctime'];?></td>
+          <td>
+            <a href="javascript:void(0)" data-url="index.php?act=showContent&fileName=<?php echo $row['name']?>" data-toggle="modal" data-target="#showContent" class="showContentBtn btn btn-info btn-sm">查看</a>&nbsp;&nbsp;
+            <a href="javascript:void(0)" data-filename="<?php echo $row['name']?>" data-url="index.php?act=editContent&fileName=<?php echo $row['name']?>" data-toggle="modal" data-target="#editContent" class="editContentBtn  btn btn-info btn-sm">编辑</a>&nbsp;&nbsp;
+            <a href="javascript:void(0)" data-filename="<?php echo $row['name']?>" data-url="index.php?act=delFile&fileName=<?php echo $row['name']?>" data-toggle="modal" data-target="#delFileConfirm" class="delFileBtn  btn btn-danger btn-sm">删除</a>&nbsp;&nbsp;
+            <a data-filename="<?php echo $row['name']?>" href="index.php?act=downFile&fileName=<?php echo $row['name']?>" class="downloadFileBtn  btn btn-info btn-sm">
+            <span class="icon icon-small "><span class="icon-download"></span></span>
+            下载
+            </a>
+          </td>
+        </tr>
+        <?php }}?>
       </table>
 
     </div> 
+    <input type="hidden" id="hiddenPath" value="<?php echo $path;?>" />
     <!--[主体结束]-->
 
 
@@ -113,7 +161,29 @@ require 'dir.inc.php';
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
-            <button type="button" id="createFileBtn" class="btn btn-primary">提交</button>
+            <a id="createFileBtn" class="btn btn-primary">提交</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal fade" id="createDir" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="exampleModalLabel">创建文件夹</h4>
+          </div>
+          <div class="modal-body">
+              <div class="form-group">
+                <label for="recipient-name" class="control-label">文件夹名称:</label>
+                <input type="text" class="form-control" id="dirName">
+                <input type="hidden" id="path" value="<?php echo $path?>" />
+              </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+            <a id="createDirBtn" class="btn btn-primary">提交</a>
           </div>
         </div>
       </div>
@@ -146,12 +216,30 @@ require 'dir.inc.php';
           <div class="modal-body">
             <form action="index.php?act=createFile" method="post" id="createFileForm">
               <div class="form-group">
-              <div class="contentTextarea">没有内容</div>
+              <div class="contentTextarea">载入中...</div>
               </div>
             </form>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal fade" id="delFileConfirm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="exampleModalLabel">删除文件</h4>
+          </div>
+          <div class="modal-body">
+              <div class="contentTextarea">确认是否真的要删除此文件,删除后不可恢复哟</div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+            <a href="" class="delFileConfirmBtn btn btn-danger" data-dismiss="modal">确认</a>
           </div>
         </div>
       </div>
@@ -174,6 +262,28 @@ require 'dir.inc.php';
           <div class="modal-footer">
             <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
             <button type="button" class="btn btn-primary editContentSubmit" data-dismiss="modal">提交</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+     <div class="modal fade" id="uploadFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="exampleModalLabel">上传文件</h4>
+          </div>
+          <div class="modal-body">
+            <form action="index.php?act=uploadFile&path=<?php echo $path;?>" method="post" id="uploadFile" enctype="multipart/form-data">
+              <div class="form-group">
+                <a class="file">选择文件<input type="file" class="form-control uploadFileInput" name="uploadFileInput" /></a>
+              </div>
+            
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+            <input type="submit" class="btn btn-primary uploadFileBtn"  value="提交"/></form>
           </div>
         </div>
       </div>
